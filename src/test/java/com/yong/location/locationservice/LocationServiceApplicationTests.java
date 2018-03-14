7023f9082adba902c7420ee05fc09aa32e946868
LiangYong <yong.a.liang@accenture.com>
2018-03-14 16:15:55 +0800

    update port

--- src/test/java/com/yong/location/locationservice/LocationServiceApplicationTests.java
+++ src/test/java/com/yong/location/locationservice/LocationServiceApplicationTests.java
@@ -1,14 +1,9 @@
 package com.yong.location.locationservice;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class LocationServiceApplicationTests {
 
 	@Test
